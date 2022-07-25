eca6cb03c4787a600342d1975990560c9f930a55
xiaoxindada <2245062854@qq.com>
2022-07-26 01:15:20 +0800

    Update code date: Asia/Shanghai: 2022-07-26-01:15 > Etc/UTC: 2022-07-25-17:15
    Signed-off-by: xiaoxindada <2245062854@qq.com>

--- jni/include/generated/flags.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#define quote(s)            #s
-#define str(s)              quote(s)
-#define MAGISK_FULL_VER     MAGISK_VERSION "(" str(MAGISK_VER_CODE) ")"
-#define NAME_WITH_VER(name) str(name) " " MAGISK_FULL_VER
-#define MAGISK_VERSION      "70fd03d5"
-#define MAGISK_VER_CODE     25101
-#define MAGISK_DEBUG        0
