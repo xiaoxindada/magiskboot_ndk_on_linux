f9f6d55f51076a966ee26f36e4cbd9d6098c799c
xiaoxindada <2245062854@qq.com>
2022-11-23 09:45:58 +0800

    Update code date: Asia/Shanghai: 2022-11-23-09:45 > Etc/UTC: 2022-11-23-01:45
    Signed-off-by: xiaoxindada <2245062854@qq.com>

--- src/include/generated/flags.h
+++ src/include/generated/flags.h
@@ -3,6 +3,6 @@
 #define str(s)              quote(s)
 #define MAGISK_FULL_VER     MAGISK_VERSION "(" str(MAGISK_VER_CODE) ")"
 #define NAME_WITH_VER(name) str(name) " " MAGISK_FULL_VER
-#define MAGISK_VERSION      "cf9957ce"
+#define MAGISK_VERSION      "ed37ddd5"
 #define MAGISK_VER_CODE     25205
 #define MAGISK_DEBUG        0
