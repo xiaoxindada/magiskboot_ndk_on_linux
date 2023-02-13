099f20799e34db6d641dd67a664e0fb42a1df9f5
xiaoxindada <2245062854@qq.com>
2023-02-13 09:42:40 +0800

    Update code date: Asia/Shanghai: 2023-02-13-09:42 > Etc/UTC: 2023-02-13-01:42
    Signed-off-by: xiaoxindada <2245062854@qq.com>

--- src/include/generated/init-rs.cpp
+++ src/include/generated/init-rs.cpp
@@ -1,9 +1,17 @@
+#include <cstdint>
+
 namespace rust {
 extern "C" {
 void rust$cxxbridge1$setup_klog() noexcept;
+
+::std::int32_t rust$cxxbridge1$print_rules_device() noexcept;
 } // extern "C"
 
 void setup_klog() noexcept {
   rust$cxxbridge1$setup_klog();
+}
+
+::std::int32_t print_rules_device() noexcept {
+  return rust$cxxbridge1$print_rules_device();
 }
 } // namespace rust
--- src/include/generated/init-rs.hpp
+++ src/include/generated/init-rs.hpp
@@ -1,5 +1,8 @@
 #pragma once
+#include <cstdint>
 
 namespace rust {
 void setup_klog() noexcept;
+
+::std::int32_t print_rules_device() noexcept;
 } // namespace rust
