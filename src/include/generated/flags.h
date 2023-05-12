6c486c9e70047d6e47abfa0a45ac772ffa84dd7e
xiaoxindada <2245062854@qq.com>
2023-05-12 09:22:25 +0800

    Update code date: Asia/Shanghai: 2023-05-12-09:22 > Etc/UTC: 2023-05-12-01:22
    Signed-off-by: xiaoxindada <2245062854@qq.com>

--- src/include/generated/flags.h
+++ src/include/generated/flags.h
@@ -3,6 +3,6 @@
 #define str(s)              quote(s)
 #define MAGISK_FULL_VER     MAGISK_VERSION "(" str(MAGISK_VER_CODE) ")"
 #define NAME_WITH_VER(name) str(name) " " MAGISK_FULL_VER
-#define MAGISK_VERSION      "1610092e"
+#define MAGISK_VERSION      "bd265105"
 #define MAGISK_VER_CODE     26101
 #define MAGISK_DEBUG        0
