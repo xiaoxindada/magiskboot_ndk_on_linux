9ab749cb6b442376d403e93a0677781bb9e628eb
xiaoxindada <2245062854@qq.com>
2024-07-18 09:21:09 +0800

    Update code date: Asia/Shanghai: 2024-07-18-09:21 > Etc/UTC: 2024-07-18-01:21
    Signed-off-by: xiaoxindada <2245062854@qq.com>

--- src/external/busybox/shell/sigisemptyset.c
+++
@@ -1,8 +0,0 @@
-#include <signal.h>
-
-int sigisemptyset(const sigset_t *set)
-{
-    sigset_t empty;
-    sigemptyset(&empty);
-    return memcmp(&empty, set, sizeof(sigset_t)) == 0;
-}
--- src/external/pcre/include/pcre2.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define PCRE2_CODE_UNIT_WIDTH 8
-
-#include "../src/pcre2.h.generic"
